df8fc9ea44d497e01c0eb2d61cecde8e28654c49
massi-css <sebiha_massinissa@etu.univ-blida.dz>
2024-07-30 01:21:50 +0100

    changed data typing in th model

--- models/RegistrationModel.js
+++ models/RegistrationModel.js
@@ -1,47 +1,55 @@
 import mongoose from "mongoose";
 
 const RegistrationSchema = new mongoose.Schema({
-    firstname:{
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  university: {
+    type: String,
+    required: true,
+  },
+  field_of_study: {
+    type: String,
+    required: true,
+  },
+  Scientific_interests: [
+    {
+      language: {
         type: String,
-        required: true
+        required: true,
+      },
+      rating: {
+        type: String,
+        required: true,
+      },
     },
-    lastname:{
-        type: String,
-        required: true
-    },
-    email:{
-        type: String,
-        required: true
-    },
-    phone:{
-        type: String,
-        required: true
-    },
-    university:{
-        type: String,
-        required: true
-    },
-    field_of_study:{
-        type: String,
-        required: true
-    },
-    Scientific_interests:{
-        type: String,
-        required: true
-    },
-    experience:{
-        type: String,
-        required: true
-    },
-    why_join:{
-        type: String,
-        required: true
-    },
-    grade_level:{
-        type: String,
-        required: true
-    }
-})
+  ],
+  experience: {
+    type: String,
+    required: true,
+  },
+  why_join: {
+    type: String,
+    required: true,
+  },
+  grade_level: {
+    type: String,
+    required: true,
+  },
+});
 
 const Registration = mongoose.model("Registration", RegistrationSchema);
 
